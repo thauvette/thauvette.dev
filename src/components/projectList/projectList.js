bf14e3278df02c4d0954488417cad2a112d06a98
thauvette <thauvette@gmail.com>
2021-09-01 16:11:21 -0300

    added tools and links to project list

--- src/components/projectList/projectList.js
+++ src/components/projectList/projectList.js
@@ -4,14 +4,13 @@ import * as sharedStyles from '../../styles/shared/sectionLists.module.scss'
 
 // const tags = ['solo', 'react', 'react native']
 // todo: update team to take array
-// todo: add links
 const projects = [
   {
     title: 'Financial Calculator Embeds',
     description:
       'Embedded widgets built with preact and d3 for use by financial advisors',
     role: 'widget guy',
-    tools: ['preact', 'd3', 'yup'],
+    tools: ['preact', 'd3', 'yup', 'css modules'],
     tags: ['react', 'preact', 'widget'],
     team: (
       <>
@@ -24,14 +23,19 @@ const projects = [
         </a>
       </>
     ),
-    links: ['https://www.gordonstirrett.com/calculators/investment-calculator'],
+    links: [
+      {
+        title: 'Gordon Stirrett',
+        url: 'https://www.gordonstirrett.com/calculators/investment-calculator',
+      },
+    ],
   },
   {
     title: 'IWNDT',
     description:
       'r/stopdrinking inspired, feature rich, customizable sobriety tracking native App, including milestones, diary, stats, and more.',
     role: 'Solo design and development',
-    tools: ['React Native', 'Redux', 'redux-persist'],
+    tools: ['React Native', 'Redux', 'redux-persist', 'typescript'],
     tags: ['solo', 'react native'],
   },
   {
@@ -71,7 +75,12 @@ const projects = [
     role: 'Front-end technical lead & lead developer',
     tools: ['React', 'Redux'],
     tags: ['VERB', 'react'],
-    links: ['https://www.ayersrockresort.com.au/book'],
+    links: [
+      {
+        title: 'Ayers Rock Resort',
+        url: 'https://www.ayersrockresort.com.au/book',
+      },
+    ],
   },
 ]
 
@@ -97,6 +106,29 @@ const ProjectList = () => (
             <p className={sharedStyles.ItemDetailsLabel}>product:</p>
             <p>{project.description}</p>
           </div>
+          {project.links && (
+            <div className={sharedStyles.ItemDetailsContainer}>
+              <p className={sharedStyles.ItemDetailsLabel}>links:</p>
+
+              {project.links.map(link => (
+                <a
+                  className={sharedStyles.ItemDetailsLink}
+                  key={link.url}
+                  href={link.url}
+                  target="_blank"
+                  rel="noreferrer noopener"
+                >
+                  {link.title}
+                </a>
+              ))}
+            </div>
+          )}
+          {project.tools && (
+            <div className={sharedStyles.ItemDetailsContainer}>
+              <p className={sharedStyles.ItemDetailsLabel}>tools:</p>
+              <p>{project.tools.join(', ')}</p>
+            </div>
+          )}
         </div>
       </div>
     ))}
